a34c131032b523ac682bbc28d5e792811e7aa914
InoueSota <k022g0045@g.neec.ac.jp>
2025-09-01 22:53:33 +0900

    リファクタリング

--- Assets/Scripts/Characters/PlayerTear.cs
+++ Assets/Scripts/Characters/PlayerTear.cs
@@ -2,49 +2,44 @@ using UnityEngine;
 
 public class PlayerTear : MonoBehaviour
 {
+    // 自コンポーネント
     private PlayerController controller;
 
+    // フラグ類
     private bool isActive;
 
     public void Initialize(PlayerController _controller)
     {
+        // 自コンポーネントの取得
         controller = _controller;
     }
 
     void Update()
     {
+        // 破り、開始
         if (!isActive && controller.IsGrounded() && Input.GetButtonDown("Special"))
         {
             controller.SetDefault();
             isActive = true;
         }
 
-        if (isActive)
+        // 十字ボタンの左右どちらかを押したら、左右どちらかを破り捨てる
+        if (isActive && (Input.GetAxis("Horizontal2") < 0f || Input.GetAxis("Horizontal2") > 0f))
         {
-            if (Input.GetAxis("Horizontal2") < 0f)
+            // 該当するFieldObjectを破る操作を行うが、破られるかどうかはAllFieldObjectManager内で判断する
+            foreach (GameObject fieldObject in GameObject.FindGameObjectsWithTag("FieldObject"))
             {
-                foreach(GameObject fieldObject in GameObject.FindGameObjectsWithTag("FieldObject"))
+                if (Input.GetAxis("Horizontal2") < 0f && fieldObject.transform.position.x < Mathf.RoundToInt(transform.position.x))
                 {
-                    if (fieldObject.transform.position.x < Mathf.RoundToInt(transform.position.x))
-                    {
-                        fieldObject.GetComponent<AllFieldObjectManager>().HitTear();
-                    }
+                    fieldObject.GetComponent<AllFieldObjectManager>().HitTear();
                 }
-
-                isActive = false;
-            }
-            else if (Input.GetAxis("Horizontal2") > 0f)
-            {
-                foreach (GameObject fieldObject in GameObject.FindGameObjectsWithTag("FieldObject"))
+                else if (Input.GetAxis("Horizontal2") > 0f && fieldObject.transform.position.x > Mathf.RoundToInt(transform.position.x))
                 {
-                    if (fieldObject.transform.position.x > Mathf.RoundToInt(transform.position.x))
-                    {
-                        fieldObject.GetComponent<AllFieldObjectManager>().HitTear();
-                    }
+                    fieldObject.GetComponent<AllFieldObjectManager>().HitTear();
                 }
-
-                isActive = false;
             }
+
+            isActive = false;
         }
     }
 
--- Assets/Scripts/Objects/AllFieldObjectManager.cs
+++ Assets/Scripts/Objects/AllFieldObjectManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class AllFieldObjectManager : MonoBehaviour
 {
+    // 該当するObjectType
     public enum ObjectType
     {
         GROUND,
@@ -9,6 +10,7 @@ public class AllFieldObjectManager : MonoBehaviour
     }
     [SerializeField] private ObjectType objectType;
 
+    // 該当する表示状態
     public enum Status
     {
         FIRST,
@@ -20,64 +22,69 @@ public class AllFieldObjectManager : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private BoxCollider2D boxCollider2D;
 
+    // ページのレイヤー番号
+    private int page1Layer;
+    private int page2Layer;
+
     void Start()
     {
         // 自コンポーネントの取得
         spriteRenderer = GetComponent<SpriteRenderer>();
         boxCollider2D = GetComponent<BoxCollider2D>();
 
+        // ページのレイヤー番号を設定
+        page1Layer = 6;
+        page2Layer = 7;
+
+        // ObjectTypeによって処理を変更
         switch (objectType)
         {
-            case ObjectType.GROUND:
-
-
-
-                break;
+            // BLOCKはページ数によって当たり判定の有無とそれに伴う表示を変更する
             case ObjectType.BLOCK:
 
-                if (transform.parent.gameObject.layer == 6)
+                // どのページでも色をまず戻す
+                spriteRenderer.color = Color.yellow;
+
+                // ページ1のとき
+                if (transform.parent.gameObject.layer == page1Layer)
                 {
-                    spriteRenderer.color = Color.yellow;
                     status = Status.FIRST;
                 }
-                else if (transform.parent.gameObject.layer == 7)
+                // ページ2のとき
+                else if (transform.parent.gameObject.layer == page2Layer)
                 {
-                    spriteRenderer.color = Color.yellow;
-                    spriteRenderer.color = new(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.2f);
-                    boxCollider2D.enabled = false;
                     status = Status.SECOND;
+                    // 半透明にする
+                    spriteRenderer.color = new(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.2f);
+                    // 当たり判定を一時的に無くす
+                    boxCollider2D.enabled = false;
                 }
 
                 break;
         }
     }
 
-    void Update()
-    {
-
-    }
-
+    /// <summary>
+    /// 破られたあとの処理
+    /// </summary>
     public void HitTear()
     {
         switch (objectType)
         {
-            case ObjectType.GROUND:
-                break;
             case ObjectType.BLOCK:
 
                 switch (status)
                 {
+                    // 最前面のときは消去する
                     case Status.FIRST:
-
                         Destroy(gameObject);
-
                         break;
-                    case Status.SECOND:
 
+                    // 最前面にする
+                    case Status.SECOND:
                         spriteRenderer.color = Color.yellow;
                         status = Status.FIRST;
                         boxCollider2D.enabled = true;
-
                         break;
                 }
 
