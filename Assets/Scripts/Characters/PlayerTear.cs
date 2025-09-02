91985bdef51d8956c9fcb7d4d13c8f2530996ddc
InoueSota <k022g0045@g.neec.ac.jp>
2025-09-02 16:34:00 +0900

    hoi

--- Assets/Scripts/Characters/PlayerTear.cs
+++ Assets/Scripts/Characters/PlayerTear.cs
@@ -12,6 +12,7 @@ public class PlayerTear : MonoBehaviour
 
     // フラグ類
     private bool isActive;
+    private bool isReleaseStick;
 
     // Global Volume
     [SerializeField] private float fadePower;
@@ -22,6 +23,8 @@ public class PlayerTear : MonoBehaviour
 
     public void Initialize(PlayerController _controller)
     {
+        isReleaseStick = true;
+
         // 自コンポーネントの取得
         controller = _controller;
 
@@ -37,20 +40,27 @@ public class PlayerTear : MonoBehaviour
         // 破り、開始
         if (!isActive && controller.IsGrounded() && Input.GetButtonDown("Special"))
         {
+            if (Input.GetAxisRaw("Horizontal") < 0f || Input.GetAxisRaw("Horizontal") > 0f)
+            {
+                isReleaseStick = false;
+            }
             controller.SetDefault();
             targetIntensity = maxIntensity;
             isActive = true;
         }
+        // 破り、終了
         else if (isActive && Input.GetButtonDown("Special"))
         {
-            // 破り、終了
             controller.SetBackToNormal();
             targetIntensity = 0f;
             isActive = false;
         }
 
+        // 指を一度話させる処理
+        if (isActive && !isReleaseStick && Input.GetAxisRaw("Horizontal") == 0f) { isReleaseStick = true; }
+
         // 十字ボタンの左右どちらかを押したら、左右どちらかを破り捨てる
-        if (isActive && (Input.GetAxisRaw("Horizontal") < 0f || Input.GetAxisRaw("Horizontal") > 0f))
+        if (isActive && isReleaseStick && (Input.GetAxisRaw("Horizontal") < 0f || Input.GetAxisRaw("Horizontal") > 0f))
         {
             // 該当するFieldObjectを破る操作を行うが、破られるかどうかはAllFieldObjectManager内で判断する
             foreach (GameObject fieldObject in GameObject.FindGameObjectsWithTag("FieldObject"))
@@ -67,6 +77,11 @@ public class PlayerTear : MonoBehaviour
 
             // 該当するレイヤーに破き情報を与える
             gridTransform.GetChild(1).GetComponent<PageManager>().SetTearInfomation(new(Mathf.RoundToInt(transform.position.x), 0f, 0f), new(Input.GetAxisRaw("Horizontal"), 0f, 0f));
+
+            // 破り、終了
+            controller.SetBackToNormal();
+            targetIntensity = 0f;
+            isActive = false;
         }
 
         // Global Volume
