bd56658cf8f7ba50bd7713c66a3ef9729a701fea
ShigaMitsuki <k022g0049@g.neec.ac.jp>
2025-09-04 02:27:47 +0900

    ブロック繋がる

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class PlayerController : MonoBehaviour
